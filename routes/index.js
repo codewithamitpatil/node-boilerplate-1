597a4aa2c32741420261d4e8ca5428b51f1ee691
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-08-12 01:23:41 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const router  = express.Router();
+
+// importing async handler to avoid try and catch
+const asyncHandler = require('../middlewares/asyncHandler');
+
+// import routes
+
+
+// intialize routes
+
+
+
+// module exports
+module.exports = router;
