cd62ed3fc72026c4b5d7bab218387f23699223e4
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-08-12 02:05:29 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,39 @@
-//temp thong
+
+HttpError = require('http-errors')
+
+// importing user model
+
+
+// Fetch by id
+const Fetch = () => {
+
+
+};
+
+// Fetch all
+const FetchAll = () => {
+
+
+};
+
+// Create
+const Create = () => {
+
+ 
+
+};
+
+// Update
+const Update = () => {
+
+
+};
+
+// Delete
+const Delete = () => {
+
+};
+
+
+// export controllers
+module.exports = { Fetch ,FetchAll , Create , Update , Delete };
